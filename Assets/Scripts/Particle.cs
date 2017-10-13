11495dc7a96171f9e81503327dbfb2ddd6ef0304
Jin Dai <fabulousdj@foxmail.com>
2017-10-13 15:57:12 -0500

    particle physics added; basic scene created

--- Assets/Scripts/Particle.cs
+++ Assets/Scripts/Particle.cs
@@ -1,16 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-public class pparticle : MonoBehaviour {
+public class Particle : MonoBehaviour {
+
+	public Vector3 Position;
+	public Vector3 Velocity;
+	public Vector3 Force;
+	public TimeSpan Age;
+	public TimeSpan MaxAge;
+	public float Mass = 1.0f;
+	public GameObject Apperance;
+
+	private Vector3 acceleration;
 
 	// Use this for initialization
 	void Start () {
-		
+		acceleration = Force / Mass;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		float deltaTime = Time.deltaTime;
+		this.Position = CalculateNewPosition(deltaTime);
+		this.Velocity = CalculateNewVelocity(deltaTime);
 	}
+
+	private Vector3 CalculateNewPosition(float deltaTime)
+	{
+		return this.Position + CalculateNewVelocity(deltaTime / 2) * deltaTime;
+	}
+
+	private Vector3 CalculateNewVelocity(float deltaTime)
+	{
+		return this.Velocity + this.acceleration * deltaTime;
+	}
+
 }
